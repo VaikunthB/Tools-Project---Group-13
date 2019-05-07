18b20b384182f6ebc2ad43667e162f174f4961cb
Vaikunth Bharadwaj <vaikunth.bharadwaj@gmail.com>
2019-05-07 01:26:19 +0000

    Unitest change

--- unittest.py
+++ unittest.py
@@ -1,6 +1,6 @@
 import unittest
 import main
-import classify
+import analyzer
 
 class TestMain(unittest.TestCase):
 
@@ -15,28 +15,28 @@ class TestMain(unittest.TestCase):
 class TestClassMethods(unittest.TestCase):
 
     def test_song_id(self):
-        self.assertEqual(classify.song_id('003~Jeff-Beck~All-Shook-Up.txt'), 003)
+        self.assertEqual(analyzer.song_id('003~Jeff-Beck~All-Shook-Up.txt'), 003)
 
     def test_song_artist(self):
-        self.assertEqual(classify.song_artist('003~Jeff-Beck~All-Shook-Up.txt'), Jeff Beck)
+        self.assertEqual(analyzer.song_artist('003~Jeff-Beck~All-Shook-Up.txt'), Jeff Beck)
 
     def test_song_title(self):
-        self.assertEqual(classify.song_title('003~Jeff-Beck~All-Shook-Up.txt'), All Shook Up)
+        self.assertEqual(analyzer.song_title('003~Jeff-Beck~All-Shook-Up.txt'), All Shook Up)
 
     def test_kid_safe(self):
-        self.assertEqual(classify.kid_safe('I like flowers. This is a kid safe song'), 1)
-        self.assertEqual(classify.kid_safe('My life is terrible. Fuck you guys, my life sucks', 0.7)
-        self.assertEqual(classify.kid_safe('Fuck you motherfucker, bitches whore moher fuck fuck you fucking fucker fuck whore bitch'), 1)
+        self.assertEqual(analyzer.kid_safe('I like flowers. This is a kid safe song'), 1)
+        self.assertEqual(analyzer.kid_safe('My life is terrible. Fuck you guys, my life sucks', 0.7)
+        self.assertEqual(analyzer.kid_safe('Fuck you motherfucker, bitches whore moher fuck fuck you fucking fucker fuck whore bitch'), 1)
 
         def test_love(self):
-        self.assertEqual(classify.love('You are the love, you are beautiful and I love you and want to kiss you and hold you'), 1)
-        self.assertEqual(classify.love('I want to move to University and chill on the beach'), 0)
+        self.assertEqual(analyzer.love('You are the love, you are beautiful and I love you and want to kiss you and hold you'), 1)
+        self.assertEqual(analyzer.love('I want to move to University and chill on the beach'), 0)
 
         def test_mood(self):
-            self.assertEqual(classify.mood('I am so happy and cheerful and energetic'), 0.9)
+            self.assertEqual(analyzer.mood('I am so happy and cheerful and energetic'), 0.9)
 
         def test_length(self): 
-            self.assertEqual(classify.length('I am small'), 0)
+            self.assertEqual(analyzer.length('I am small'), 0)
 
         if __unit__ = '__main__': 
             unittest.main()
